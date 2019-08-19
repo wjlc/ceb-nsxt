64b888f6f22ebabdef288b4b9248240487449121
wjlc <jlwei@alauda.io>
2019-08-20 00:18:25 +0800

    update

--- src/test/java/io/alauda/nsxt/NsxtApplicationTests.java
+++ src/test/java/io/alauda/nsxt/NsxtApplicationTests.java
@@ -1,7 +1,9 @@
 package io.alauda.nsxt;
 
+import io.alauda.nsxt.controller.LocalizationController;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +11,12 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class NsxtApplicationTests {
 
+	@Autowired
+	public LocalizationController localizationController;
+
 	@Test
 	public void contextLoads() {
+		System.out.println(localizationController.getIpPools());
 	}
 
 }
